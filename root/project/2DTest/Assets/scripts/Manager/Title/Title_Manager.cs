87ce8df41fa7869b2d9851b7f326afeb60e16d90
坂本友希 <sw.style0312@gmail.com>
2016-07-17 00:14:10 +0900

    再アップ

---
+++ root/project/2DTest/Assets/scripts/Manager/Title/Title_Manager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Title_Manager : MonoBehaviour
+{
+	private bool isFirstSetting=false; 
+	private bool isSwitchScene=false;
+
+	// Use this for initialization
+	void Start ()
+	{
+		CameraFade.StartAlphaFade(Color.black,true,1,0,()=>{isFirstSetting=true;});
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if(!isFirstSetting||isSwitchScene){return;}
+
+		if(Input.GetKeyDown(KeyCode.Return))
+		{
+			isSwitchScene=true;
+			CameraFade.StartAlphaFade(Color.black,false,1,0,()=>{SceneManager.LoadScene("Game");});
+		}
+	}
+}
---
+++ root/project/2DTest/Assets/scripts/Utilty/MyMath.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+static public class MyMath
+{
+	static public float MapValues(float x,float inMin,float inMax,float outMin,float outMax)
+	{
+		return ((x-inMin)*(outMax-outMin)/(inMax-inMin)+outMin);
+	}
+
+	static public float Fps()
+	{
+		return (1.0f / Time.deltaTime);
+	}
+}
